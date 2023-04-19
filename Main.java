5b4e6b05a50fa611d18686ccc1c3815b4933692c
karton675 <127328589+karton675@users.noreply.github.com>
2023-04-19 14:29:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+   int wysokosc;
+    wysokosc = 36; 
+    for(int i=0; i<wysokosc; i+=1
+       ){ //o ile zwiekszyc kolejny wiersz
+      for(int j =0; j<=i; j++){
+        System.out.print("+");}
+      System.out.println();
+      }
+    
+    }
+  
+}
